9df96652c436dbe4c1b732ea2d1542dd4304511e
Boris <275691@list.ru>
2019-01-25 20:14:55 +0500

    Для ноута

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-'use strict';
+"use strict"
+var money = prompt("Ваш бюджет на месяц?")
+var time = prompt("Введите дату в формате YYYY-MM-D")
+var appData = {
+    budjet: money,
+    timedata: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings:false
+};
+
+for (let i=0; i<2; i++) {
+    let a=prompt("Введите обязательную статью расходов в этом месяце"),
+        b=prompt("Во сколько обойдется?");
+
+    appData.expenses[a] = b
+}
 
 
-let x = "sssd",
-    y = "ss";
+alert(appData.budjet/30);
 
-
-console.log(x || y)
+console.log(appData)
