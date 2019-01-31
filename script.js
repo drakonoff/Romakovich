8031698062d72472e69af7925ac93088766f5ca6
Boris <275691@list.ru>
2019-01-31 13:25:16 +0500

    nastolka new

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-"use strict";
+"use string";
+var kubik=[];
+var pers=0;
+for (var i = 0; i <= 12; i++) {
+	kubik[i]=-6+i;
+}
 
-var pickRandomWord = function (words) {
-    return words[Math.floor(Math.random() * words.length)];
-    };
-
-var randomWords = ["Планета", "Червяк", "Цветок", "Компьютер"];
-
-
-console.log(pickRandomWord(randomWords))
+while(pers<20){
+	if(pers<0){
+		pers=0;
+		document.write("_0_сектор. ")
+	} else{
+		var kubNumber=Math.floor(Math.random()*12);
+		pers+=kubik[kubNumber];
+		document.write("Ты сейчас на этой клетке - "+ pers+"<br>");
+		
+	}
+}
+var vvodite=prompt("Какой лучше?")
+var h=$("#klaus")
+h.text(vvodite)
+h.fadeout(3000)
+$("h1").fadeOut(3000);
+$("body").append("<p>Хэппи энд, поздравляем</p>")
